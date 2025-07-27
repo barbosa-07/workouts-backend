93cfba9ad3da8002fae27353c42a67a239873699
Rizwan Akram <rizwanakram4049@gmail.com>
2025-07-27 16:20:37 +0530

    chore: migrations | store

--- internal/app/app.go
+++ internal/app/app.go
@@ -9,6 +9,7 @@ import (
 
 	"github.com/Rizzwaan/workoutVerse/internal/api"
 	"github.com/Rizzwaan/workoutVerse/internal/store"
+	"github.com/Rizzwaan/workoutVerse/migrations"
 )
 
 type Application struct {
@@ -21,6 +22,11 @@ func NewApplication() (*Application, error) {
 	pgDB, err := store.Open()
 	if err != nil {
 		return nil, err
+	}
+
+	err = store.MigrateFS(pgDB, migrations.FS, ".")
+	if err != nil {
+		panic(err)
 	}
 
 	logger := log.New(os.Stdout, "", log.Ldate|log.Ltime)
--- internal/store/database.go
+++ internal/store/database.go
@@ -3,8 +3,10 @@ package store
 import (
 	"database/sql"
 	"fmt"
+	"io/fs"
 
 	_ "github.com/jackc/pgx/v4/stdlib"
+	"github.com/pressly/goose/v3"
 )
 
 func Open() (*sql.DB, error) {
@@ -19,4 +21,28 @@ func Open() (*sql.DB, error) {
 
 	fmt.Println("Connected to Database...")
 	return db, nil
+}
+
+func MigrateFS(db *sql.DB, migrationFS fs.FS, dir string) error {
+	goose.SetBaseFS(migrationFS)
+
+	defer func() {
+		goose.SetBaseFS(nil)
+	}()
+	return Migrate(db, dir)
+}
+
+func Migrate(db *sql.DB, dir string) error {
+	dialectErr := goose.SetDialect("postgres")
+	if dialectErr != nil {
+		return fmt.Errorf("db: migrate %w", dialectErr)
+	}
+
+	gooseErr := goose.Up(db, dir)
+	if gooseErr != nil {
+		return fmt.Errorf("goose up: %w", gooseErr)
+	}
+	fmt.Println("Database migration completed successfully.")
+
+	return nil
 }
